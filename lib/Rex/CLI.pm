16e95c0b96e701496cf922a1b1e7626b6229bb8f
Ferenc Erki <erkiferenc@gmail.com>
2019-12-10 20:17:41 +0100

    perltidy

--- lib/Rex/CLI.pm
+++ lib/Rex/CLI.pm
@@ -284,7 +284,7 @@ CHECK_OVERWRITE: {
 
     Rex::global_sudo(0);
     Rex::Logger::debug("Removing lockfile") if ( !exists $opts{'F'} );
-    CORE::unlink("$::rexfile.lock") if ( !exists $opts{'F'} );
+    CORE::unlink("$::rexfile.lock")         if ( !exists $opts{'F'} );
     CORE::exit 0;
   }
 
@@ -782,7 +782,7 @@ sub load_rexfile {
     my @lines = split( $/, $e );
 
     Rex::Logger::info( "Compile time errors:", 'error' );
-    Rex::Logger::info( "\t$_", 'error' ) for @lines;
+    Rex::Logger::info( "\t$_",                 'error' ) for @lines;
 
     exit 1;
   }
@@ -795,7 +795,7 @@ sub exit_rex {
 
   Rex::global_sudo(0);
   Rex::Logger::debug("Removing lockfile") if !exists $opts{'F'};
-  unlink("$::rexfile.lock") if !exists $opts{'F'};
+  unlink("$::rexfile.lock")               if !exists $opts{'F'};
 
   select STDOUT;
 
